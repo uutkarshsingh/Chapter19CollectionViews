4c531f25df60cdc5ae65649c56a9203a43daa02a
utkarshSinghRathor <uutkarshsingh@gmail.com>
2016-11-15 17:07:13 +0530

    Added the Picker to the application

---
+++ Chapter19CollectionViews/Chapter19CollectionViewsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Chapter19CollectionViews
+{
+	public partial class Chapter19CollectionViewsPage : ContentPage
+	{
+		public Chapter19CollectionViewsPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
---
+++ Chapter19CollectionViews/PickerView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace Chapter19CollectionViews
+{
+	public partial class PickerView : ContentPage
+	{
+		public PickerView()
+		{
+			InitializeComponent();
+		}
+
+		public void OnPickerSelectedIndexChanged(object sender, EventArgs args)
+		{
+			System.Diagnostics.Debug.WriteLine("The selected index has changed  {0} ",localPicker.SelectedIndex);
+
+			PropertyInfo propertyInfo = typeof(Keyboard).GetRuntimeProperty(localPicker.Items[localPicker.SelectedIndex]);
+
+			entry.Keyboard = (Keyboard)propertyInfo.GetValue(null);
+		}
+	}
+}
